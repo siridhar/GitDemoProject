972a429768b02c61dcca1cd7b8ee2c8f26f75d4a
Microgrid <Microgrid@DESKTOP-C34ESC4>
2025-01-10 14:02:38 +0530

    This is my first commit

---
+++ src/GitDemoProject.java
@@ -0,0 +1,16 @@
+
+public class GitDemoProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String work= "yes";
+		
+		if(work=="yes")
+			
+System.out.println("I'm working in MICROGRID");
+		else
+			System.out.println("I'm not working");	
+
+	}
+
+}
